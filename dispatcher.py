07b5897c89b3f75e09b77ca24becc91f7f98786c
AnatolyVarkus <164738391+AnatolyVarkus@users.noreply.github.com>
2024-07-12 09:01:22 +0700

    Add files via upload

---
+++ dispatcher.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, Request, Depends, Query, HTTPException, WebSocket
+from urllib.parse import unquote
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse, Response
+from fastapi.middleware.cors import CORSMiddleware
+import config
+import subprocess
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+processes = {}
+
+class DeployRoomRequest(BaseModel):
+    token: str
+    guid: str
+    port: str
+    bid: float
+    player_amount: int
+
+class RemoveRoomRequest(BaseModel):
+    token: str
+    guid: str
+
+@app.post('/deployroom')
+async def deployRoom(request: DeployRoomRequest):
+
+    if request.token == config.TOKEN:
+        try:
+            process = subprocess.Popen([f"C:\\Users\Administrator\Desktop\StandaloneWindows64\StandaloneWindows64.exe "
+                                        f"-serverId='{request.guid}' "
+                                        f"-port='{request.port}' "
+                                        f"-maxPlayers='{request.player_amount}' "
+                                        f"-isFree='{True if request.bid == 0 else False}'"])
+            processes[request.guid] = process
+            return True
+        except:
+            return False
+    else:
+        raise HTTPException(status_code=400, detail="Incorrect token")
+
+
+@app.post('/removeroom')
+async def removeRoom(request: RemoveRoomRequest):
+    if request.token == config.TOKEN:
+        try:
+            last_process = processes.pop(request.guid)
+            last_process.terminate()
+            return True
+        except:
+            return False
+    else:
+        raise HTTPException(status_code=400, detail="Incorrect token")
